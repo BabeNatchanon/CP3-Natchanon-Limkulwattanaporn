d90a855896d51df3df743041d53310eb3837c223
BabeNatchanon <80640451+BabeNatchanon@users.noreply.github.com>
2021-05-04 10:35:46 +0700

    Add files via upload

---
+++ Lecture105_Natchanon_L.py
@@ -0,0 +1,29 @@
+class Vehicle:
+    licenseNumber = ""
+    serialCode = ""
+    def turnOnAirConditioner(self):
+        print("Turn on : Air")
+
+class Car(Vehicle):
+    licenseNumber = ""
+    serialCode = ""
+    def turnOnAirConditioner(self):
+        print("Turn on : Air")
+
+class PickUp(Vehicle):
+    licenseNumber = ""
+    serialCode = ""
+    def turnOnAirConditioner(self):
+        print("Turn on : Air")
+
+class Van(Vehicle):
+    licenseNumber = ""
+    serialCode = ""
+    def turnOnAirConditioner(self):
+        print("Turn on : Air")
+
+class EstateCar(Vehicle):
+    licenseNumber = ""
+    serialCode = ""
+    def turnOnAirConditioner(self):
+        print("Turn on : Air")
